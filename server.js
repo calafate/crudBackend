4d12003c0e016dfd7d7e12c03b161032f11c6fa2
calafate <107583529+MirthaCalafate@users.noreply.github.com>
2023-02-01 19:36:06 -0300

    actualizaciones

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 require("dotenv").config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
